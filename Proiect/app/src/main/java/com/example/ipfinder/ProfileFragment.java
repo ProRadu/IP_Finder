4dcd64b39a8b20c461a1e31cbbc6a463f48bed16
ProRadu <radu.nicolae.trascalie@gmail.com>
2023-06-10 12:50:11 +0300

    Added new functionality
    The user can now set a profile picture and it will be uploaded to firebase storage

--- Proiect/app/src/main/java/com/example/ipfinder/ProfileFragment.java
+++ Proiect/app/src/main/java/com/example/ipfinder/ProfileFragment.java
@@ -1,18 +1,25 @@
 package com.example.ipfinder;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -20,8 +27,14 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.concurrent.Executor;
+
+import io.grpc.Context;
 
 
 public class ProfileFragment extends Fragment {
@@ -32,6 +45,8 @@ public class ProfileFragment extends Fragment {
     private TextView textViewEmail, textViewUserProfile;
     private FirebaseUser user;
     private String userID;
+    private ImageView profileImage;
+    private StorageReference storageReference;
 
     public ProfileFragment(){super(R.layout.fragment_profile);}
 
@@ -46,10 +61,8 @@ public class ProfileFragment extends Fragment {
         textViewUserProfile=view.findViewById(R.id.user_profile);
         user=auth.getCurrentUser();
 
-
-
-
-
+        profileImage = view.findViewById(R.id.profile_image);
+        storageReference = FirebaseStorage.getInstance().getReference();
 
 
         if(user == null){
@@ -68,6 +81,14 @@ public class ProfileFragment extends Fragment {
                     }
                 }
             });
+
+            StorageReference profileRef = storageReference.child("users/"+userID+"/profile.jpg");
+            profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Picasso.get().load(uri).into(profileImage);
+                }
+            });
         }
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -78,7 +99,47 @@ public class ProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                //profileImage.setImageURI(imageUri);
 
+                uploadImageToFirebase(imageUri);
+            }
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        StorageReference fileRef = storageReference.child("users/"+userID+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity().getApplicationContext(),"Image Uploaded", Toast.LENGTH_LONG).show();
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profileImage);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity().getApplicationContext(),"Upload failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
